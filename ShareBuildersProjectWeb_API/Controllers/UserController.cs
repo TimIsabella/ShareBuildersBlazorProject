afc7d79ebd67394f961d467cc4af7baf323db798
TimIsabella <timjisabella@gmail.com>
2022-11-06 00:15:30 -0700

    Pending add

---
+++ ShareBuildersProjectWeb_API/Controllers/UserController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ShareBuildersProject_Business;
+
+namespace ShareBuildersProjectWeb_API.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class UserController : ControllerBase
+	{
+		private IUserRepository _userRepository;
+
+		public UserController(IUserRepository userRepository)
+		{
+			_userRepository = userRepository;
+		}
+	}
+}
